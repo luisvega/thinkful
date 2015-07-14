32816fab902f5426a9db6ed237c8d478cebae379
Luis Vega <luis@port9.com>
2015-07-13 20:43:27 -0700

    project 5.2.3

--- unit_5/pictionary/public/main.js
+++ unit_5/pictionary/public/main.js
@@ -3,25 +3,64 @@ var pictionary = function() {
   var socket = io();
   var drawing = false;
   var guessBox = $('#guess input');
-
+  var selector = $('#selector');
+  var gameRole;
+  var wordToDraw;
   var onKeyDown = function(event) {
     if (event.keyCode != 13) {
       return;
     }
     /*
-      example giving by thinkfule guessBox.value() doesn't seem to work
+      example given by thinkful guessBox.value() doesn't seem to work
       had to change to guessBox.val()
     */
     var guess = guessBox.val();
     socket.emit('guess', guess);
     guessBox.val('');
+    $('#guesses').text(guess)
   }
 
+  selector.on('change', function(){
+    var selected = $(this).val();
+    socket.emit('role', selected);
+    if (selected === 'drawer') {
+      gameRole = 'drawer'
+      $('#register').hide();
+      $('#meh').show();
+      $('#guessOutput').show();
+    }
+
+  })
+  socket.on('role', function(role){
+    $('#register').hide();
+    $('#meh').show();
+    $('#guesser').show();
+    $('#guessOutput').show();
+    $('#wordToDraw').hide();
+    gameRole = role
+  })
   guessBox.on('keydown', onKeyDown)
   socket.on('guess', function(attempt){
+    var attemptedWord
     $('#guesses').text(attempt)
   });
 
+  socket.on('new drawer', function(data){
+    gameRole = 'drawer'
+    $('#guesser').hide();
+    $('#wordToDraw').show();
+    $('#meh').show();
+    $('#guessOutput').show();
+  })
+
+  socket.on('wordToDraw', function(data){
+    if(gameRole === 'drawer') {
+      console.log(data)
+      $('#wordToDraw').show();
+      wordToDraw = data.word;
+      $('#theWord').text(wordToDraw);
+    }
+  })
   var draw = function(position) {
     context.beginPath();
     context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
@@ -30,10 +69,16 @@ var pictionary = function() {
 
   canvas = $('canvas');
   context = canvas[0].getContext('2d');
+  context.clearRect(0,0,canvas.width,canvas.height)
   canvas[0].width = canvas[0].offsetWidth;
   canvas[0].height = canvas[0].offsetHeight;
+
+
   canvas.on('mousedown', function(event){
-    drawing = true;
+    if (gameRole === 'drawer') {
+      drawing = true;
+    }
+
   });
   canvas.on('mouseup', function(event){
     drawing = false;
@@ -44,7 +89,6 @@ var pictionary = function() {
       var position = { x: event.pageX - offset.left,
                        y: event.pageY - offset.top};
       socket.emit('draw', position)
-      console.log(position)
       draw(position);
     }
   });
@@ -52,9 +96,6 @@ var pictionary = function() {
     draw(position)
   })
 };
-
-
-
 
 
 $(document).ready(function(){
--- unit_5/pictionary/server.js
+++ unit_5/pictionary/server.js
@@ -9,16 +9,61 @@ app.use(express.static('public'))
 var server = http.Server(app)
 var io = socket_io(server)
 
+var wordToGuess
+
 io.on('connection', function(socket){
+
+  function setNewWord() {
+    wordToGuess = WORDS[Math.floor(Math.random() * WORDS.length)]
+    socket.emit('wordToDraw', { word: wordToGuess })
+    console.log('wordToDraw')
+  }
   console.log('client connected!')
   socket.on('draw', function(position){
     socket.broadcast.emit('draw', position)
   })
   socket.on('guess', function(guess){
-    socket.broadcast.emit('guess', guess)
+    if (guess === wordToGuess) {
+      console.log(guess)
+      socket.broadcast.emit('guess', guess)
+      socket.broadcast.emit('role', { role: 'guesser' })
+      socket.emit('new drawer', guess)
+      setNewWord()
+    } else {
+      socket.broadcast.emit('guess', guess)
+    }
+
+  })
+
+  socket.on('role', function(role){
+    socket.role = role
+    if (role === 'drawer'){
+      drawer = true
+    }
+    // http://bit.ly/1O2qkR4
+    socket.broadcast.emit('role', { role: 'guesser' })
+    setNewWord()
   })
 })
 
 var port = 8080
 console.log('listening on ', port)
 server.listen(port)
+
+
+
+var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
